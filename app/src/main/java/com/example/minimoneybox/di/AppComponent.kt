6be03f701963fb22ab45ac46c0e4f02c68f6fbf2
gustavo <gustavo.maia@b2b.com.pt>
2020-11-07 18:24:08 +0000

    AppComponent interface and app class

---
+++ app/src/main/java/com/example/minimoneybox/di/AppComponent.kt
@@ -0,0 +1,21 @@
+package com.example.minimoneybox.di
+
+import android.app.Application
+import com.example.minimoneybox.App
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.AndroidInjector
+import javax.inject.Singleton
+
+
+@Singleton
+@Component(modules = [AndroidInjectionModule::class, ActivityBuildersModule::class, ViewModelFactoryModule::class, NetworkModule::class])
+interface AppComponent : AndroidInjector<App> {
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application?): Builder?
+        fun build(): AppComponent?
+    }
+}
