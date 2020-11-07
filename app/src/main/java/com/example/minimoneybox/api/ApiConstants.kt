051f56026bc7a97b250299d781c515ed6e2ffad8
gustavo <gustavo.maia@b2b.com.pt>
2020-11-07 16:18:06 +0000

    Api constants class

---
+++ app/src/main/java/com/example/minimoneybox/api/ApiConstants.kt
@@ -0,0 +1,13 @@
+package com.example.minimoneybox.api
+
+class ApiConstants {
+
+    companion object {
+        const val BASE_URL = "https://api-test01.moneyboxapp.com/"
+        const val APP_ID = "3a97b932a9d449c981b595"
+        const val CONTENT_TYPE = "application/json"
+        const val APP_VERSION = "5.10.0"
+        const val API_VERSION = "3.0.0"
+    }
+
+}
--- app/src/main/java/com/example/minimoneybox/ui/LoginActivity.kt
+++ app/src/main/java/com/example/minimoneybox/ui/LoginActivity.kt
@@ -1,10 +1,11 @@
-package com.example.minimoneybox
+package com.example.minimoneybox.ui
 
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.lottie.LottieAnimationView
+import com.example.minimoneybox.R
 import com.google.android.material.textfield.TextInputLayout
 
 /**
