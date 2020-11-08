bc6b2f86c11a69295e3aca4122b5f12dd96fcd2b
gustavo <gustavo.maia@b2b.com.pt>
2020-11-08 16:26:24 +0000

    ProductsBinding

---
+++ app/src/main/java/com/example/minimoneybox/dataBinding/ProductsBinding.kt
@@ -0,0 +1,35 @@
+package com.example.minimoneybox.dataBinding
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.minimoneybox.api.models.Product
+import com.example.minimoneybox.ui.login.adapters.ProductRecyclerAdapter
+
+
+class ProductsBinding {
+
+    @BindingAdapter("products")
+    fun setProducts(view: RecyclerView, items: List<Product>) {
+
+        if (items == null) {
+            return
+        }
+        val layoutManager = view.layoutManager
+
+        if (layoutManager == null) {
+            view.layoutManager = LinearLayoutManager(view.context)
+        }
+
+
+        var adapter: ProductRecyclerAdapter = view.adapter as ProductRecyclerAdapter
+
+        if (adapter == null) {
+            adapter = ProductRecyclerAdapter(view.context, items.toMutableList())
+            view.adapter = adapter
+        } else {
+            adapter.update(items)
+        }
+    }
+
+}
