e0f786f03dd27bc61496177a7ba902bb93bdd8fc
gustavo <gustavo.maia@b2b.com.pt>
2020-11-08 16:20:30 +0000

    ProductRecyclerAdapter

---
+++ app/src/main/java/com/example/minimoneybox/ui/login/adapters/ProductRecyclerAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.minimoneybox.ui.login.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.minimoneybox.R
+import com.example.minimoneybox.api.models.Product
+import com.example.minimoneybox.databinding.ItemProductBinding
+
+
+class ProductRecyclerAdapter(val context : Context, var products : MutableList<Product>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding: ItemProductBinding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.item_product, parent, false)
+        return ProductViewHolder(binding.getRoot())
+    }
+
+    override fun getItemCount(): Int {
+        return products.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        var item = products.get(position)
+
+        (holder as ProductViewHolder).binding.product = item
+        (holder as ProductViewHolder).binding.executePendingBindings()
+    }
+
+    fun update(items: List<Product>) {
+        products.clear()
+        products.addAll(items)
+        notifyDataSetChanged()
+    }
+}
